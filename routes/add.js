4d9e8adcf166b4d50a3ba3951e9126b15b83234d
M3ahmed <m3ahmed@ucsd.edu>
2021-02-04 10:35:19 -0800

    almost done

--- routes/add.js
+++ routes/add.js
@@ -2,4 +2,15 @@ var data = require("../data.json");
 
 exports.addFriend = function(request, response) {    
 	// Your code goes here
+	res.render('add', data);
+
+	var newFriend = {
+		'name': req.query.name,
+		'description': req.query.description,
+		'imageURL': 'http://lorempixel.com/500/500/people'
+	}
+
+	console.log(newFriend.name);
+	console.log(newFriend.description);
+	data["friends"].push(newFriend);
  }
